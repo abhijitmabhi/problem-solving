cf9560eb3ee6c1f509d4ccebfd98690c7a23ebe5
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-01-02 17:21:34 +0100

    Feat/set mismatch645 (#2)
    * 42: continue
    
    * 645: Set Mismatch solved

---
+++ src/main/java/com/ama/TrappingRainWater42.java
@@ -0,0 +1,16 @@
+package com.ama;
+
+public class TrappingRainWater42 {
+    public static void main(String[] args) {
+        System.out.println(trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+
+    public static int trap(int[] height) {
+        int trapWaterAmount = 0;
+
+
+
+        return trapWaterAmount;
+    }
+
+}
