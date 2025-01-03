e389a5f2e654709dab22553f84fdbea25da9523a
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-01-03 18:44:37 +0100

    42: TrappingRainWater solved

--- src/main/java/com/ama/TrappingRainWater42.java
+++ src/main/java/com/ama/TrappingRainWater42.java
@@ -2,15 +2,30 @@ package com.ama;
 
 public class TrappingRainWater42 {
     public static void main(String[] args) {
-        System.out.println(trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+        System.out.println(trap(new int[]{4,2,0,3,2,5}));
     }
 
     public static int trap(int[] height) {
-        int trapWaterAmount = 0;
+        int left = 0;
+        int right = height.length-1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int trapWater = 0;
 
+        while (left < right){
+            leftMax = Math.max(leftMax,height[left]);
+            rightMax = Math.max(rightMax,height[right]);
 
+            if (height[left] < height[right]){
+                trapWater += leftMax - height[left];
+                left++;
+            }else {
+                trapWater += rightMax - height[right];
+                right--;
+            }
+        }
 
-        return trapWaterAmount;
+        return trapWater;
     }
 
 }
