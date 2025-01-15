1f1e1538acfa4a4618f698e7a76740921e2d1029
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-01-15 21:44:13 +0100

    15: Three3Sum solved

---
+++ src/main/java/com/ama/MaxConsecutiveOnesIII1004.java
@@ -0,0 +1,11 @@
+package com.ama;
+
+public class MaxConsecutiveOnesIII1004 {
+    public static void main(String[] args) {
+        longestOnes(new int[]{1,1,1,0,0,0,1,1,1,1,0}, 2);
+    }
+
+    public static int longestOnes(int[] nums, int k) {
+        return 0;
+    }
+}
