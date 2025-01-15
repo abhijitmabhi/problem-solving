c9e613bdabed1482b55e15786c6262f88e4e7aba
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-01-15 16:46:39 +0100

    686: RepeatedStringMatch solved

--- src/main/java/com/ama/RepeatedStringMatch686.java
+++ src/main/java/com/ama/RepeatedStringMatch686.java
@@ -2,21 +2,21 @@ package com.ama;
 
 public class RepeatedStringMatch686 {
     public static void main(String[] args) {
-        System.out.println(repeatedStringMatch("a", "a"));
+        System.out.println(repeatedStringMatch("abcd", "cdabcdab"));
     }
 
     public static int repeatedStringMatch(String a, String b) {
         int repeated = 1;
         StringBuilder sBuilder = new StringBuilder(a);
 
-        while (!sBuilder.toString().contains(b) && (sBuilder.length() < b.length()+a.length())){
+        while (!sBuilder.toString().contains(b) && (sBuilder.length() < b.length() + a.length())) {
             sBuilder.append(a);
             repeated++;
         }
 
-        if(sBuilder.toString().contains(b)){
+        if (sBuilder.toString().contains(b)) {
             return repeated;
-        }else {
+        } else {
             return -1;
         }
     }
