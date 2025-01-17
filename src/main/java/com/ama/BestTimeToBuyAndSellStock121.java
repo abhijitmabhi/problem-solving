34eace2757a80fde8f2e055a91d046c5557e801d
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-01-17 23:37:58 +0100

    121: maxProfit - solved

--- src/main/java/com/ama/BestTimeToBuyAndSellStock121.java
+++ src/main/java/com/ama/BestTimeToBuyAndSellStock121.java
@@ -2,19 +2,19 @@ package com.ama;
 
 public class BestTimeToBuyAndSellStock121 {
     public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
     }
 
     public static int maxProfit(int[] prices) {
         int profit = 0;
         int buyPrice = prices[0];
 
-        for (int i=1; i < prices.length; i++){
-           if(buyPrice > prices[i]){
-               buyPrice = prices[i];
-           }
+        for (int i = 1; i < prices.length; i++) {
+            if (buyPrice > prices[i]) {
+                buyPrice = prices[i];
+            }
 
-           profit = Math.max(prices[i] - buyPrice, profit);
+            profit = Math.max(prices[i] - buyPrice, profit);
         }
 
         return profit;
