558ef4d6882dd8661756b91716b2071013d07cee
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-01-17 18:35:03 +0100

    15: Three3Sum - solved

--- src/main/java/com/ama/Three3Sum15.java
+++ src/main/java/com/ama/Three3Sum15.java
@@ -6,39 +6,44 @@ import java.util.List;
 
 public class Three3Sum15 {
     public static void main(String[] args) {
-        threeSum(new int[]{0,0,0,0});
+        System.out.println(threeSum(new int[]{-2,0,1,1,2}));
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
-        var result = new ArrayList<List<Integer>>();
+        List<List<Integer>> result = new ArrayList<>();
+
+        if(Arrays.stream(nums).allMatch(x -> x == 0)){
+            result.add(List.of(0,0,0));
+
+            return result;
+        }
+
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
             var triplet = new ArrayList<Integer>();
-            int leftPtr = i + 1;
-            int rightPtr = nums.length - 1;
+            var leftPtr = i + 1;
+            var rightPtr = nums.length - 1;
 
             while (leftPtr != rightPtr) {
                 var tempSum = nums[i] + nums[leftPtr] + nums[rightPtr];
+
                 if (tempSum != 0) {
                     if (tempSum < 0) {
                         leftPtr++;
                     } else {
                         rightPtr--;
                     }
-
                 } else {
-                    triplet.add(nums[i]);
-                    triplet.add(nums[leftPtr]);
-                    triplet.add(nums[rightPtr]);
-
-                    result.add(triplet);
-                    break;
+                    triplet.addAll(List.of(nums[i], nums[leftPtr], nums[rightPtr]));
+                    result.add(new ArrayList<>(triplet));
+                    leftPtr++;
+                    triplet.clear();
                 }
             }
         }
 
-        return result;
+        return result.stream().distinct().toList();
 
     }
 }
