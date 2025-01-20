3d5e1dc2a9e2967089354ca10b1c5781d6eeb724
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-01-21 00:19:51 +0100

    39: CombinationSum - solved

--- src/main/java/com/ama/CombinationSum39.java
+++ src/main/java/com/ama/CombinationSum39.java
@@ -8,19 +8,19 @@ public class CombinationSum39 {
         System.out.println(combinationSum(new int[]{2, 3, 5}, 8));
     }
 
-    public static List<List<Integer>> combinationSum(int [] candidates, int target){
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(candidates,target,0,new ArrayList<>(), result);
+        backtrack(candidates, target, 0, new ArrayList<>(), result);
         return result;
 
     }
 
-    private static void backtrack(int[] candidates, int target, int start, List<Integer> current, List<List<Integer>> result){
-        if (target == 0){
+    private static void backtrack(int[] candidates, int target, int start, List<Integer> current, List<List<Integer>> result) {
+        if (target == 0) {
             result.add(new ArrayList<>(current));
         }
 
-        if(target < 0){
+        if (target < 0) {
             return;
         }
 
