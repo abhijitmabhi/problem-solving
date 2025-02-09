9e7eeb174fe7f2d906996f5eb422e04402f61ba0
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-02-09 18:32:25 +0100

    3: LongestSubstringWithoutRepeatingCharacters - updated

--- src/main/java/com/ama/LongestSubstringWithoutRepeatingCharacters3.java
+++ src/main/java/com/ama/LongestSubstringWithoutRepeatingCharacters3.java
@@ -1,60 +1,29 @@
 package com.ama;
 
-import java.util.Iterator;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 
 public class LongestSubstringWithoutRepeatingCharacters3 {
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("dvdf"));
+        System.out.println(lengthOfLongestSubstring(""));
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        if (s == null || s.isEmpty())
-            return 0;
+        HashSet<Character> myHash = new HashSet<>();
+        int leftPtr = 0;
+        int rightPtr = 0;
+        int maxLength = 0;
 
-        if (s.isBlank() || s.length() == 1)
-            return 1;
-
-        LinkedHashSet<Character> myHash = new LinkedHashSet<>();
-        int longestSubstringLength = Integer.MIN_VALUE;
-        int i = 0;
-
-        while (i < s.length()) {
-            if (!myHash.add(s.charAt(i))) {
-                longestSubstringLength = Math.max(myHash.size(), longestSubstringLength);
-                int duplicateIndex = getIndex(myHash, s.charAt(i));
-                removeItemsUpToIndex(myHash,duplicateIndex);
+        while (rightPtr < s.length()) {
+            if (!myHash.contains(s.charAt(rightPtr))) {
+                myHash.add(s.charAt(rightPtr));
+                rightPtr++;
+                maxLength = Math.max(myHash.size(), maxLength);
             } else {
-                i++;
-            }
-
-            if (i == s.length()) {
-                longestSubstringLength = Math.max(myHash.size(), longestSubstringLength);
+                myHash.remove(s.charAt(leftPtr));
+                leftPtr++;
             }
         }
 
-        return longestSubstringLength;
-    }
-
-    public static int getIndex(LinkedHashSet<Character> set, char target) {
-        int index = 0;
-        for (Character character : set) {
-            if (character == target) {
-                return index;
-            }
-            index++;
-        }
-        return -1;
-    }
-
-    public static void removeItemsUpToIndex(LinkedHashSet<Character> set, int indexToRemoveUpTo) {
-        Iterator<Character> iterator = set.iterator();
-        int currentIndex = 0;
-
-        while (iterator.hasNext() && currentIndex <= indexToRemoveUpTo) {
-            iterator.next();
-            iterator.remove();
-            currentIndex++;
-        }
+        return maxLength;
     }
 }
