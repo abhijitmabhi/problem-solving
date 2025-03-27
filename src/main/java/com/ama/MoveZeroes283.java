b83014cc8d009fc9041fd7e30dcc82ec67c4195f
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-03-27 22:00:52 +0100

    80: MoveZeroes - solved

--- src/main/java/com/ama/MoveZeroes283.java
+++ src/main/java/com/ama/MoveZeroes283.java
@@ -2,15 +2,15 @@ package com.ama;
 
 public class MoveZeroes283 {
     public static void main(String[] args) {
-        moveZeroes(new int[]{1,1,0,2,5});
+        moveZeroes(new int[]{1, 1, 0, 2, 5});
     }
 
-    public static void moveZeroes(int[] nums){
+    public static void moveZeroes(int[] nums) {
         int ptrZero = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0){
-                if(i != ptrZero){
+            if (nums[i] != 0) {
+                if (i != ptrZero) {
                     nums[ptrZero] = nums[i];
                     nums[i] = 0;
                 }
