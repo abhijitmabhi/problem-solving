530955203af1d218ab1a884971e8d470209bf414
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-05-25 20:46:24 +0200

    202: HappyNumber - solved

--- src/main/java/com/ama/HappyNumber202.java
+++ src/main/java/com/ama/HappyNumber202.java
@@ -6,18 +6,13 @@ import java.util.List;
 
 public class HappyNumber202 {
     public static void main(String[] args) {
-        System.out.println(isHappyNumber(2));
+        System.out.println(isHappyNumber(19));
     }
 
     private static boolean isHappyNumber(int n) {
-        boolean happyNumber = false;
         HashSet<Integer> track = new HashSet<>();
 
-        while (!happyNumber) {
-
-            if (track.contains(n)) {
-                return false;
-            }
+        while (!track.contains(n)) {
             track.add(n);
 
             List<Integer> nums = getNumList(n);
@@ -27,19 +22,14 @@ public class HappyNumber202 {
                 sum += (long) i * i;
             }
 
-            if (sum >= Integer.MAX_VALUE) {
-                return false;
-            }
-
             if (sum == 1) {
-                happyNumber = true;
+                return true;
             }
 
-            nums.clear();
             n = (int) sum;
         }
 
-        return happyNumber;
+        return false;
     }
 
     private static List<Integer> getNumList(int n) {
