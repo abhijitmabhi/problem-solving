371cd16bf125dc2446184cff535184b09f08a760
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-06-02 10:51:34 +0200

    167: TwoSumIIInputArrayIsSorted167 - solved

--- src/main/java/com/ama/TwoSumIIInputArrayIsSorted167.java
+++ src/main/java/com/ama/TwoSumIIInputArrayIsSorted167.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 public class TwoSumIIInputArrayIsSorted167 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(twoSumII(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(twoSumII2(new int[]{2, 7, 11, 15}, 9)));
     }
 
     private static int[] twoSumII(int[] numbers, int target) {
@@ -22,5 +22,32 @@ public class TwoSumIIInputArrayIsSorted167 {
         }
 
         return new int[]{};
+
+        // Time: O(N)
+        // Space: O(N)
+
+        // To_Do
+        //*Not considering non-decreasing array inputs
+        // use two pinters to overcome this issue
+    }
+
+    private static int[] twoSumII2(int[] numbers, int target) {
+        int first = 0;
+        int last = numbers.length - 1;
+
+        while (first < last) {
+            int sum = numbers[first] + numbers[last];
+
+            if (sum == target) {
+                return new int[]{first + 1, last + 1};
+            } else if (sum < target) {
+                first++;
+            } else {
+                last--;
+            }
+        }
+
+        return new int[]{};
+
     }
 }
