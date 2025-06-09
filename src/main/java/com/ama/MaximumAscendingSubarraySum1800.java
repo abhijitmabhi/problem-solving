c2f40a127b037a82cdafddc25c9b54a1ab2616db
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-06-09 22:54:33 +0200

    1800: MaximumAscendingSubarraySum - improved

--- src/main/java/com/ama/MaximumAscendingSubarraySum1800.java
+++ src/main/java/com/ama/MaximumAscendingSubarraySum1800.java
@@ -2,15 +2,15 @@ package com.ama;
 
 public class MaximumAscendingSubarraySum1800 {
     public static void main(String[] args) {
-        System.out.println(maxAscendingSum(new int[]{100, 10, 1}));
+        System.out.println(maxAscendingSum(new int[]{10,20,30,5,10,50}));
     }
 
     private static int maxAscendingSum(int[] nums) {
         int sum = 0;
         int max = 0;
 
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if(i == nums.length -1 || nums[i] < nums[i+1]){
+        for (int i = 0; i < nums.length; i++) {
+            if(i == 0 || nums[i] > nums[i-1]){
                 sum += nums[i];
             } else {
                 sum = nums[i];
