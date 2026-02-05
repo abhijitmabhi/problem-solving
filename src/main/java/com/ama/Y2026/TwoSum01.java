b7333dcd15ec9116392cfdb18db99f0edae16a32
Abhijit Mondal Abhi <abhijit.abhi@n26.com>
2026-02-06 00:56:49 +0100

    twoSum - solved

---
+++ src/main/java/com/ama/Y2026/TwoSum01.java
@@ -0,0 +1,24 @@
+package com.ama.Y2026;
+
+import java.util.Arrays;
+import java.util.Hashtable;
+
+public class TwoSum01 {
+    static void main() {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+
+    static int[] twoSum(int[] nums, int target) {
+        Hashtable<Integer, Integer> map = new Hashtable<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(target - nums[i])) {
+                map.put(nums[i], i);
+            } else {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+        }
+
+        return new int[]{};
+    }
+}
