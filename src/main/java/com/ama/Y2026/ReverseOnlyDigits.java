b431402403cdcc46713d4a0b55a12a1afa5d7bf8
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2026-02-08 10:40:10 +0100

    ReverseOnlyDigits - solved (#102)
    * ReverseOnlyDigits - solved
    
    * - downgrade java
    - install spotless

---
+++ src/main/java/com/ama/Y2026/ReverseOnlyDigits.java
@@ -0,0 +1,34 @@
+package com.ama.Y2026;
+
+public class ReverseOnlyDigits {
+    static void main() {
+        System.out.println(reverseDigits("123"));
+    }
+
+    private static String reverseDigits(String str) {
+        char[] strArr = str.toCharArray();
+
+        int start = 0;
+        int end = strArr.length - 1;
+
+        while (start < end) {
+            if (Character.isDigit(strArr[start]) && Character.isDigit(strArr[end])) {
+                char temp = strArr[start];
+                strArr[start] = strArr[end];
+                strArr[end] = temp;
+
+                start++;
+                end--;
+            } else if (Character.isDigit(strArr[start])) {
+                end--;
+            } else if (Character.isDigit(strArr[end])) {
+                start++;
+            } else {
+                start++;
+                end--;
+            }
+        }
+
+        return String.valueOf(strArr);
+    }
+}
