7eef22188282b272d35a43406f29aab65128d92d
Abhijit Mondal Abhi <abhijit.abhi@n26.com>
2026-02-08 01:48:21 +0100

    BestTimeToBuyAndSellStock121 - solved

---
+++ src/main/java/com/ama/Y2026/BestTimeToBuyAndSellStock121.java
@@ -0,0 +1,23 @@
+package com.ama.Y2026;
+
+public class BestTimeToBuyAndSellStock121 {
+    static void main() {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int buyPrice = Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            if (price < buyPrice) {
+                buyPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - buyPrice);
+            }
+        }
+
+        return maxProfit;
+
+    }
+}
