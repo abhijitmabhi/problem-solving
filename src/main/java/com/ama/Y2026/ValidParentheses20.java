f1f1fac7ce84a73a40b4fbbdd9d924181540cf47
Abhijit Mondal Abhi <abhijit.abhi@n26.com>
2026-02-08 15:18:07 +0100

    - spotless

--- src/main/java/com/ama/Y2026/ValidParentheses20.java
+++ src/main/java/com/ama/Y2026/ValidParentheses20.java
@@ -10,15 +10,15 @@ public class ValidParentheses20 {
     public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
-        for(char c : s.toCharArray()){
-            if(c == '('){
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
                 stack.push(')');
             } else if (c == '{') {
                 stack.push('}');
             } else if (c == '[') {
                 stack.push(']');
             } else {
-                if(stack.isEmpty() || c != stack.pop()){
+                if (stack.isEmpty() || c != stack.pop()) {
                     return false;
                 }
             }
