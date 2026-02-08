f79dca3f499a9bc94db90d5b270e7d338d02305d
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2026-02-08 01:28:44 +0100

    Feat/2026 (#100)
    * twoSum - solved
    
    * twoSum - solved
    
    * handson

---
+++ src/main/java/com/ama/Y2026/HandsOn.java
@@ -0,0 +1,29 @@
+package com.ama.Y2026;
+
+import java.util.List;
+
+public class HandsOn {
+    static void main() {
+        var myList = List.of("Alex", "Bob", "Charlie");
+
+        myList.stream()
+                .filter(s -> s.length() > 3)
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+
+
+    }
+
+    static String handle(Result result){
+        return switch (result) {
+            case Success success -> success.value();
+            case Failure failure -> failure.error();
+        };
+    }
+}
+
+
+sealed interface Result permits Success, Failure {}
+
+record Success(String value) implements Result {}
+record Failure(String error) implements Result {}
