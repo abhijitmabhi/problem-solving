2b3baab3b91222600e2f0ff956c280a5b7a35863
abhijit <abhi.m.abhijit@gmail.com>
2026-03-05 12:31:21 +0100

    - ValidParentheses2

---
+++ src/main/java/com/ama/Y2026/ValidParentheses2.java
@@ -0,0 +1,29 @@
+package com.ama.Y2026;
+
+import java.util.Stack;
+
+public class ValidParentheses2 {
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}("));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else {
+                if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
