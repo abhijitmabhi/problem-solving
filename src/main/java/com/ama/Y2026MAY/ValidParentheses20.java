afa123e91700f7c3f400f19c4b31902b5e265a98
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2026-05-18 23:17:54 +0200

    - ValidParentheses20 (#138)

---
+++ src/main/java/com/ama/Y2026MAY/ValidParentheses20.java
@@ -0,0 +1,29 @@
+package com.ama.Y2026MAY;
+
+import java.util.Stack;
+
+public class ValidParentheses20 {
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}"));
+    }
+
+    private static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else {
+                if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
