396c12af92798cda3d8800230dcc3f3dfb03f811
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2026-05-31 13:18:06 +0200

    1: TwoSum1 - solved (#86)
    * (prob): Two Sum
    
    * (refactor): refactoring code to use HashMap

---
+++ src/main/java/com/ama/Y2026JUNE/TwoSum01.java
@@ -0,0 +1,24 @@
+package com.ama.Y2026JUNE;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum01 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
