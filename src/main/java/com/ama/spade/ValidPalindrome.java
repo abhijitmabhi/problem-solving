c434239a168f25055c93e99ee675834bef21c1cd
abhijit <abhi.m.abhijit@gmail.com>
2026-07-02 22:05:56 +0200

    - Improved palindrome check

--- src/main/java/com/ama/spade/ValidPalindrome.java
+++ src/main/java/com/ama/spade/ValidPalindrome.java
@@ -1,20 +1,23 @@
 package com.ama.spade;
 
 public class ValidPalindrome {
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
     public boolean isPalindrome(String s) {
         if (s == null) {
             throw new IllegalArgumentException("Invalid input");
         }
 
-        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        //s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
 
         int left = 0;
         int right = s.length() - 1;
 
         while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
             left++;
             right--;
         }
