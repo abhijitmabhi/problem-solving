ed09a44ce0e411a86975583020adabf03fc0cb49
abhijit <abhi.m.abhijit@gmail.com>
2026-07-24 21:58:05 +0200

    - FindAllNumbersDisappearedInAnArray

---
+++ src/main/java/com/ama/remind/FindAllNumbersDisappearedInAnArray448.java
@@ -0,0 +1,32 @@
+package com.ama.remind;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllNumbersDisappearedInAnArray448 {
+    public static void main(String[] args) {
+        System.out.println(findDisappearedNumbers(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
+    }
+
+    // Time: O(n)
+    // Space: O(n)
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int num : nums) {
+            int index = Math.abs(num) - 1;
+
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+
+        return result;
+    }
+}
