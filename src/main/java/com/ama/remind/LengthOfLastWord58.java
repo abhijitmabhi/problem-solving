96839b8436014eca1feb41f07e620235e9e94d38
abhijit <abhi.m.abhijit@gmail.com>
2026-07-25 09:46:41 +0200

    - LengthOfLastWord

--- src/main/java/com/ama/remind/LengthOfLastWord58.java
+++ src/main/java/com/ama/remind/LengthOfLastWord58.java
@@ -2,19 +2,24 @@ package com.ama.remind;
 
 public class LengthOfLastWord58 {
     public static void main(String[] args) {
-        System.out.println(lengthOfLastWord("noon"));
+        System.out.println(lengthOfLastWord("look at the noon "));
     }
 
     // Time: O(n)
     // Space: O(1)
     public static int lengthOfLastWord(String s) {
-        s = s.strip();
         int right = s.length() - 1;
+        int count = 0;
 
-        while (right > -1 && s.charAt(right) != ' ') {
+        while (right >= 0 && s.charAt(right) == ' ') {
             right--;
         }
 
-        return s.length() - 1 - right;
+        while (right >= 0 && s.charAt(right) != ' ') {
+            count++;
+            right--;
+        }
+
+        return count;
     }
 }
