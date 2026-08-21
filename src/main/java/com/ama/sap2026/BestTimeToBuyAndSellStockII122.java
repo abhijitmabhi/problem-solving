ad992bbccf07615cbfb8c70ec250414ecb568db5
abhijit <abhi.m.abhijit@gmail.com>
2026-08-21 11:06:24 +0200

    - BestTimeToBuyAndSellStockII122

---
+++ src/main/java/com/ama/sap2026/BestTimeToBuyAndSellStockII122.java
@@ -0,0 +1,22 @@
+package com.ama.sap2026;
+
+public class BestTimeToBuyAndSellStockII122 {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+
+    static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int buyingPrice = Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            if (price > buyingPrice) {
+                maxProfit += price - buyingPrice;
+            }
+            buyingPrice = price;
+
+        }
+
+        return maxProfit;
+    }
+}
