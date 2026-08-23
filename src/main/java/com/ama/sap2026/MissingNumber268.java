58ec50ecb943a51f7fa48abadba49cef0fd59092
abhijit <abhi.m.abhijit@gmail.com>
2026-08-23 17:00:03 +0200

    - MissingNumber2

--- src/main/java/com/ama/sap2026/MissingNumber268.java
+++ src/main/java/com/ama/sap2026/MissingNumber268.java
@@ -2,7 +2,7 @@ package com.ama.sap2026;
 
 public class MissingNumber268 {
     public static void main(String[] args) {
-        System.out.println(missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}));
+        System.out.println(missingNumber2(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}));
     }
 
     private static int missingNumber(int[] nums) {
@@ -15,6 +15,25 @@ public class MissingNumber268 {
         for (int n = 0; n < nums.length; n++) {
             if (numArr[n] == 0) {
                 return n;
+            }
+        }
+
+        return -1;
+    }
+
+    private static int missingNumber2(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < n) {
+                int curNum = Math.abs(nums[i]);
+                nums[curNum] = -nums[curNum];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return i;
             }
         }
 
